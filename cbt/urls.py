b6a46ad43ab90df90a9160bc9ad99d2921352916
CodeLuminary <victorijoni@yahoo.com>
2021-10-20 19:24:05 +0100

    Routing added

---
+++ cbt/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/api/get-users', views.getUser),
+    path('/api/add-users', views.addUser),
+    path('/api/add-exams', views.addExam),
+    path('/api/get-exams', views.getExams),
+    path('/api/add-exam-questions', views.addExamQuestion),
+    path('/api/get-exam-questions', views.getExamQuestions),
+    path('/account/login',views.userLogin),
+    path('/api/login-users', views.login),
+]
--- cbt/views.py
+++ cbt/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def getUsers(request):
+    pass
+def addUser(request):
+    pass
+def addExam(request):
+    pass
+def getExams(request):
+    pass
+def addExamQuestion(request):
+    pass
+def getExamQuestions(request):
+    pass
+def userLogin(request):
+    pass
+def login(request):
+    pass
