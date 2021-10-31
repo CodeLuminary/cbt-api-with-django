f6d98aa90b2da3504662c71c6ad15a15c14c0945
CodeLuminary <victorijoni@yahoo.com>
2021-10-31 16:24:34 +0100

    Login page added

--- cbt/views.py
+++ cbt/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 def getUsers(request):
     pass
@@ -16,6 +19,7 @@ def addExamQuestion(request):
 def getExamQuestions(request):
     pass
 def userLogin(request):
+    return render(request,'login.html')
     pass
 def login(request):
     pass
--- cbtapi/settings.py
+++ cbtapi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'cbtapi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['cbt/Template','cbt'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
